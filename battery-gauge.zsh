7b2b7f0d8590482026b6e58e1a75992fc97a6605
sudorook <daemon@nullcodon.com>
2018-04-29 21:39:01 -0400

    add function to print circular battery gauge

--- battery-gauge.zsh
+++ battery-gauge.zsh
@@ -47,6 +47,55 @@ function battery_pct_prompt() {
   fi
 }
 
+function battery_level_circlegauge() {
+  local color_green=${BATTERY_COLOR_GREEN:-%F{green}};
+  local color_yellow=${BATTERY_COLOR_YELLOW:-%F{yellow}};
+  local color_red=${BATTERY_COLOR_RED:-%F{red}};
+  local color_reset=${BATTERY_COLOR_RESET:-%{%f%k%b%}};
+  local battery_prefix=${BATTERY_GAUGE_PREFIX:-''};
+  local battery_suffix=${BATTERY_GAUGE_SUFFIX:-''};
+  local filled_symbol=${BATTERY_GAUGE_FILLED_SYMBOL:-'●'};
+  local threefourths_symbol=${BATTERY_GAUGE_THREEFOURTHS_SYMBOL:-'◕'};
+  local half_symbol=${BATTERY_GAUGE_HALF_SYMBOL:-'◑'};
+  local onefourth_symbol=${BATTERY_GAUGE_ONEFOURTH_SYMBOL:-'◔'};
+  local empty_symbol=${BATTERY_GAUGE_EMPTY_SYMBOL:-'○'};
+  local charging_color=${BATTERY_CHARGING_COLOR:-$color_yellow};
+  local charging_symbol=${BATTERY_CHARGING_SYMBOL:-'⚡'};
+
+  local battery_remaining_percentage=$(battery_pct);
+
+  if [[ $battery_remaining_percentage =~ [0-9]+ ]]; then
+    if (( $battery_remaining_percentage >= 88 )); then
+      circlegauge=$filled_symbol
+    elif (( $battery_remaining_percentage >= 63 )); then
+      circlegauge=$threefourths_symbol
+    elif (( $battery_remaining_percentage >= 38 )); then
+      circlegauge=$half_symbol
+    elif (( $battery_remaining_percentage >= 13 )); then
+      circlegauge=$onefourth_symbol
+    else
+      circlegauge=$empty_symbol
+    fi
+    if (( $battery_remaining_percentage >= 50 )); then
+      gauge_color=$color_green
+    elif (( $battery_remaining_percentage >= 20 )); then
+      gauge_color=$color_yellow
+    else
+      gauge_color=$color_red
+    fi
+  else
+    gauge_color=$color_green
+    circlegauge=${BATTERY_UNKNOWN_SYMBOL:-''};
+    charging_symbol=${BATTERY_CHARGING_SYMBOL:-''};
+  fi
+
+  local charging='' && battery_is_charging && charging=$charging_symbol;
+
+  printf ' '${battery_prefix//\%/\%\%}${gauge_color//\%/\%\%}$circlegauge
+  printf ${color_reset//\%/\%\%}${battery_suffix//\%/\%\%}${color_reset//\%/\%\%}
+  printf ${charging_color//\%/\%\%}$charging${color_reset//\%/\%\%}
+}
+
 function battery_level_gauge() {
   local gauge_slots=${BATTERY_GAUGE_SLOTS:-5};
   local green_threshold=${BATTERY_GREEN_THRESHOLD:-2};
