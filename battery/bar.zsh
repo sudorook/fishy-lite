e19fad5fdb9ebdd443feafdbc10f9bc98850e4ff
sudorook <daemon@nullcodon.com>
2018-06-07 11:40:00 -0400

    copy over macos version of battery fxns

--- battery/bar.zsh
+++ battery/bar.zsh
@@ -2,9 +2,32 @@
 # Create prompts for displaying battery levels.
 #
 
-function battery_is_charging() {
-  ! [[ $(acpi 2>/dev/null | sed -n 1p | grep -c '^Battery.*Discharging') -gt 0 ]]
-}
+if [[ "$OSTYPE" = darwin* ]] ; then
+  function battery_is_charging() {
+    [[ $(ioreg -rc "AppleSmartBattery"| grep '^.*"IsCharging"\ =\ ' | sed -e 's/^.*"IsCharging"\ =\ //') == "Yes" ]]
+  }
+
+  function battery_pct() {
+    local smart_battery_status="$(ioreg -rc "AppleSmartBattery")"
+    typeset -F maxcapacity=$(echo $smart_battery_status | grep '^.*"MaxCapacity"\ =\ ' | sed -e 's/^.*"MaxCapacity"\ =\ //')
+    typeset -F currentcapacity=$(echo $smart_battery_status | grep '^.*"CurrentCapacity"\ =\ ' | sed -e 's/^.*CurrentCapacity"\ =\ //')
+    integer i=$(((currentcapacity/maxcapacity) * 100))
+    echo $i
+  }
+elif [[ "$OSTYPE" = linux* ]] ; then
+  function battery_is_charging() {
+    ! [[ $(acpi 2>/dev/null | sed -n 1p | grep -c '^Battery.*Discharging') -gt 0 ]]
+  }
+
+  function battery_pct() {
+    if (( $+commands[acpi] )) ; then
+      echo "$(acpi 2>/dev/null | sed -n 1p | cut -f2 -d ',' | tr -cd '[:digit:]')"
+    fi
+  }
+else
+  function battery_is_charging() {}
+  function battery_pct() {}
+fi
 
 function battery_charging() {
   local color_yellow=${BATTERY_COLOR_YELLOW:-%F{yellow}};
@@ -14,12 +37,6 @@ function battery_charging() {
   local charging='' && battery_is_charging && charging=${charging_symbol};
 
   printf ${charging_color//\%/\%\%}$charging${color_reset//\%/\%\%}
-}
-
-function battery_pct() {
-  if (( $+commands[acpi] )) ; then
-    echo "$(acpi 2>/dev/null | sed -n 1p | cut -f2 -d ',' | tr -cd '[:digit:]')"
-  fi
 }
 
 function battery_level_gauge() {
